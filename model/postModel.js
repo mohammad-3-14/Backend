751fb2379463bcb793fa543dea2fef524ea766ca
mohammad <mh.hashemi@netafraz.com>
2024-12-18 15:58:17 +0330

    ✨ Add slug to model

--- model/postModel.js
+++ model/postModel.js
@@ -1,39 +1,52 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["article", "video", "audio"],
+    },
+    thumbnail: {
+      type: String,
+    },
+    slug: { type: String, required: true, unique: true }, // نامک
+
+    audioUrl: {
+      type: String,
+    },
+    videoUrl: {
+      type: String,
+    },
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    // updatedAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  type: {
-    type: String,
-    required: true,
-    enum: ["article", "video", "audio"],
-  },
-  thumbnail: {
-    type: String,
-  },
-  audioUrl: {
-    type: String,
-  },
-  videoUrl: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
+  { timestamps: true }
+);
+
+postSchema.pre("save", function (next) {
+  if (!this.slug) {
+    this.slug = slugify(this.title, { lower: true });
+  }
+
+  next();
 });
 
 const post = mongoose.model("post", postSchema);
