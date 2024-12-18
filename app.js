20dead04e01b8b9447369bb82df7ee796244d9d4
mohammad <mh.hashemi@netafraz.com>
2024-12-18 15:55:53 +0330

    🎨 Use error handeling

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
+const routes = require("./routes");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 
 connectDB();
 
-const port = process.env.APP_PORT || 3000;
+app.use("/api", routes);
+app.use(errorHandler);
 
+const port = process.env.APP_PORT || 3000;
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
