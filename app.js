451ca5e51b7bc41acdc7cff3e3c3593010402694
mohammad <mh.hashemi@netafraz.com>
2024-12-18 13:32:07 +0330

    :sparkles: connect app to mongo

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/database");
+
 const app = express();
 
+connectDB();
 
-
-
-
-const port= process.env.APP_PORT || 3000;
-
- 
+const port = process.env.APP_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
