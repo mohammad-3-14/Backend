d79f87592ed920ac3c579f7d4a915a82fb02e36c
mohammad <mh.hashemi@netafraz.com>
2024-12-22 13:04:48 +0330

    🐛 Add parser for request body

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
 connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 
 app.use("/api", routes);
 app.use(errorHandler);
+
+
 
 const port = process.env.APP_PORT || 3000;
 app.listen(port, () => {
