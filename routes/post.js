340f4aa2d301a881be3893c22ed06fc8a10c4bf4
mohammad <mh.hashemi@netafraz.com>
2024-12-22 13:07:27 +0330

    ✨ Add create post method

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const slugify = require("slugify");
 const postModel = require("../model/postModel");
+const {
+  postValidationSchema,
+  validatePost,
+} = require("../validators/postValidator");
 
-router.get("/", async (req, res, next) => {
+const getAllPosts = async (req, res, next) => {
   try {
     const posts = await postModel.find();
     res.status(200).json({
@@ -12,25 +17,60 @@ router.get("/", async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-});
+};
 
-router.get("/:id", async (req, res, next) => {
+const getPostBySlug = async (req, res, next) => {
   try {
-    const post = await postModel.findById(req.params.id);
+    const post = await postModel.findOne({ slug: req.params.slug });
 
     if (post) {
-      res.status(200).json({
+      return res.status(200).json({
         message: "مقاله با موفیت دریافت شد",
         data: post,
       });
-    } else {
-      res.status(404).json({
-        message: "مقاله پیدا نشد",
-      });
     }
+    return res.status(404).json({
+      message: "مقاله پیدا نشد",
+    });
   } catch (error) {
     next(error);
   }
-});
+};
+
+const createPost = async (req, res, next) => {
+  try {
+    const slug = req.body.slug;
+
+    if (slug) {
+      const isSlugExist = await postModel.findOne({
+        slug: slugify(slug, { lower: true }),
+      });
+
+      if (isSlugExist) {
+        return res.status(400).json({
+          message: " نامک انتخابی تکراری می باشد",
+        });
+      }
+    }
+
+    const newPost = new postModel({
+      ...req.body,
+    });
+
+    await newPost.save();
+
+    res.status(201).json({
+      message: "مقاله با موفقیت ایجاد شد",
+      data: newPost,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+router.get("/", getAllPosts);
+router.get("/:slug", getPostBySlug);
+
+router.post("/", validatePost(postValidationSchema), createPost);
 
 module.exports = router;
