19e82c91431b8cf04e60bd831bc111d56c1b0b58
mohammad <mh.hashemi@netafraz.com>
2024-12-23 15:28:20 +0330

    ✨ Add update post controller and route

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,10 +3,20 @@ const postModel = require("../model/postModel");
 
 const getAllPosts = async (req, res, next) => {
   try {
-    const posts = await postModel.find();
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const posts = await postModel.find().skip(skip).limit(limit);
+    const totalPosts = await postModel.countDocuments();
+    const talPages = Math.ceil(totalPosts / limit);
+
     res.status(200).json({
       message: "مقالات با موفیت دریافت شد",
       data: posts,
+      totalPosts,
+      page,
+      perPage: limit,
     });
   } catch (error) {
     next(error);
@@ -73,9 +83,51 @@ const createPost = async (req, res, next) => {
   }
 };
 
+const updatePost = async (req, res, next) => {
+  try {
+    const { slug: currentSlug } = req.params;
+
+    const existingPost = await postModel.findOne({ slug: currentSlug });
+    if (!existingPost) {
+      return res.status(404).json({
+        message: "مقاله‌ای با این نامک یافت نشد",
+      });
+    }
+
+    let newSlug = currentSlug;
+
+    if (req.body.slug) {
+      newSlug = slugify(req.body.slug, { lower: true });
+
+      const isSlugExist = await postModel.findOne({ slug: newSlug });
+      if (
+        isSlugExist &&
+        isSlugExist._id.toString() !== existingPost._id.toString()
+      ) {
+        return res.status(400).json({
+          message: "نامک انتخابی تکراری است. لطفا یک نامک دیگر انتخاب کنید.",
+        });
+      }
+    }
+
+    const updatedPost = await postModel.findOneAndUpdate(
+      { slug: currentSlug },
+      { ...req.body, slug: newSlug },
+      { new: true, runValidators: true }
+    );
+
+    res.status(200).json({
+      message: "مقاله با موفقیت به‌روزرسانی شد",
+      data: updatedPost,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   getAllPosts,
   getPostBySlug,
   createPost,
+  updatePost,
 };
--- routes/post.js
+++ routes/post.js
@@ -4,17 +4,25 @@ const router = express.Router();
 const {
   postValidationSchema,
   validatePost,
+  updatePostValidationSchema,
+  validateUpdatePost,
 } = require("../validators/postValidator");
 
 const {
   getAllPosts,
   getPostBySlug,
   createPost,
+  updatePost,
 } = require("../controllers/postController");
 
 router.get("/", getAllPosts);
 router.get("/:slug", getPostBySlug);
 
 router.post("/", validatePost(postValidationSchema), createPost);
+router.put(
+  "/:slug",
+  validateUpdatePost(updatePostValidationSchema),
+  updatePost
+);
 
 module.exports = router;
